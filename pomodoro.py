eada01273373027381a6b2a4a43ece76666bb06d
Ran-n <ran-n@tutanota.com>
2020-07-05 12:15:23 +0200

    cambios

--- pomodoro.py
+++ pomodoro.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# by Ran#
+#+ Autor:	Ran#
+#+ Creado:	2019
+#+ Editado:	05/07/2020 12:13:05
 # -----------------------------------------------------------------
 import time
 import platform
 if platform.system()=='Windows':
-	import winsound
+    import winsound
+else:
+    import os
 # -----------------------------------------------------------------
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 500  # Set Duration To 1000 ms == 1 second
@@ -13,6 +17,9 @@ agora = time.time()
 ini = agora
 estudos = 1
 descansos = 1
+dur_estudo = 1500
+dur_descanso = 300
+
 # -----------------------------------------------------------------
 def countdown(tempo):
     while tempo:
@@ -24,20 +31,25 @@ def countdown(tempo):
         tempo -= 1
 # -----------------------------------------------------------------
 while True:
-	input('Comezar?')
-	countdown(1500)
-	if platform.system()=='Windows':
-		winsound.Beep(frequency, duration)
-	print('fin estudo ', estudos, end='')
+        input('Comezar')
+        countdown(dur_estudo)
+        if platform.system()=='Windows':
+            winsound.Beep(frequency, duration)
+        else:
+            os.system("speaker-test -t sine -f 1000 -l 1 -p 1 2&> /dev/null")
+        
+        print('fin estudo ', estudos, end='')
 
-	estudos += 1
-	input()
+        estudos += 1
+        input()
 
-	countdown(300)
-	if platform.system()=='Windows':
-		print('fin descanso ', descansos, end='')
-	winsound.Beep(frequency, duration)
-	
-	descansos +=1
-	input()
+        countdown(dur_descanso)
+        print('fin descanso ', descansos, end='')
+        if platform.system()=='Windows':
+            winsound.Beep(frequency, duration)
+        else:
+            os.system("speaker-test -t sine -f 1000 -l 1 -p 1 2&> /dev/null")
+        
+        descansos +=1
+        input()
 # -----------------------------------------------------------------
